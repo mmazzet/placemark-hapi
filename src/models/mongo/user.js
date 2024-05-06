098266f77507d803f1d750d7cb22ef664800c062
Marianna Mazzette <20104120@mail.wit.ie>
2024-05-06 11:33:12 +0100

    Initial commit

---
+++ src/models/mongo/user.js
@@ -0,0 +1,13 @@
+
+import Mongoose from "mongoose";
+
+const { Schema } = Mongoose;
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+});
+
+export const UserMongoose = Mongoose.model("User", userSchema);
